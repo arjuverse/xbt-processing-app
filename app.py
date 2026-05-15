0d5578f3c83ec39336ac9079ef959be1cda26752
Arjun K Sabu <83239835+arjuverse@users.noreply.github.com>
2026-05-15 15:19:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import seaborn as sns
+
 
 from scipy.interpolate import interp1d
 
@@ -17,7 +17,7 @@ import zipfile
 import re
 import os
 
-sns.set_style("darkgrid")
+
 
 # =====================================================
 # PAGE CONFIG
